c63d3c86ba2a1cb323cf6865151bbbf64e84470f
Deivid Santiago Ordonez Varela <deordone@car15s1.42barcelona.com>
2024-02-13 10:39:54 +0100

    update make

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,13 +6,13 @@
 /*   By: deordone <deordone@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 10:28:14 by deordone          #+#    #+#             */
-/*   Updated: 2024/02/13 10:28:49 by deordone         ###   ########.fr       */
+/*   Updated: 2024/02/13 10:35:10 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-
+# include <phtread.h>
 
 #endif
