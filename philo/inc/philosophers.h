02cebcb3b25cdac5233e03e965c3bb12801184b2
deo <>
2024-05-13 16:30:17 +0200

    reorganization of everything

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: deordone <deordone@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 10:28:14 by deordone          #+#    #+#             */
-/*   Updated: 2024/05/13 16:14:26 by droied           ###   ########.fr       */
+/*   Updated: 2024/05/13 16:29:43 by droied           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,6 @@
 # include "ph_macros.h"
 # include "ph_struct.h"
 
-/*
- * ph_lst
- * */
-//void ft_phlst_add_back(t_philo **lst, t_philo *node);
-int *ft_phlst_size(t_philo **lst);
-t_philo *ft_newph(int index);
-t_philo *ft_ph_lastnode(int index);
-t_philo **ft_phlst(t_ph_info *data);
 int parser(int argc, char **argv, t_ph_info *data);
 
 #endif
