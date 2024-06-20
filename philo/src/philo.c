3de86dfe3f38141c2e09a2cf0fed8825422ac4e5
Deivid Santiago Ordoñez Varela <deordone@cbr8s4.42barcelona.com>
2024-06-20 14:33:27 +0200

    Final Commit

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,18 +6,27 @@
 /*   By: deordone <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 13:05:49 by deordone          #+#    #+#             */
-/*   Updated: 2024/06/20 14:00:01 by deordone         ###   ########.fr       */
+/*   Updated: 2024/06/20 14:32:38 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 #include <stdio.h>
 
-static int	one_philo(t_table *t)
+static int	special_case(t_table *t)
 {
-	printf("0 The philosopher 🗿 [0] is %s\n", STR_THINK);
-	ft_usleep(t->info.time2_die);
-	printf("%li The philosopher 🗿 [0] is %s\n", t->info.time2_die, STR_DIE);
+	if (t->info.max_eat == 0)
+	{
+		printf("Every Philo is Satisfied 😋\n");
+		return (-1);
+	}
+	else if (t->info.n_philo == 1)
+	{
+		printf("0 The philosopher 🗿 [0] is %s\n", STR_THINK);
+		ft_usleep(t->info.time2_die);
+		printf("%li The philosopher 🗿 [0] is %s\n", t->info.time2_die, STR_DIE);
+		return (-1);
+	}
 	return (0);
 }
 
@@ -45,23 +54,20 @@ int	main(int ac, char **av)
 	if (ac == 6 || ac == 5)
 	{
 		if (parser(ac, av, &table) < 0)
-		{
 			printf("philo : invalid digit\n");
-			return (1);
+		else 
+		{	
+			if (special_case(&table) < 0)
+				return (-1);
+			if (init_mutex(&table) < 0)
+				return (-1);
+			if (init_philo(&table) < 0)
+				return (-1);
+			if (init_spider(&table) < 0)
+				return (-1);
 		}
-		if (table.info.n_philo == 1)
-			return (one_philo(&table));
-		if (init_mutex(&table) < 0)
-			return (-1);
-		if (init_philo(&table) < 0)
-			return (-1);
-		if (init_spider(&table) < 0)
-			return (-1);
 	}
 	else
-	{
 		printf("philo : incorrect number of arguments\n");
-		return (1);
-	}
 	return (0);
 }
